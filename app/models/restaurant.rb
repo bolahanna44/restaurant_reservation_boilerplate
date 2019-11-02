c93bdd65e3dda70d76bff5025a37a630cedeefe9
poula <bola.hanna55@gmail.com>
2019-11-02 18:40:32 +0400

    add validations to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reservations
   has_many :guests, through: :reservations
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :opening_hours, presence: true
+  validates :cuisines, presence: true
+  validates :email, presence: true
 end
