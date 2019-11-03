0e0ebf693eb7f264f8d4cb95af0debe2ef5b3e24
poula <bola.hanna55@gmail.com>
2019-11-03 16:47:13 +0400

    add spec for restaurant and reservation controller

--- spec/controllers/api/v1/reservations_controller_spec.rb
+++ spec/controllers/api/v1/reservations_controller_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ReservationsController, type: :controller do
+  describe "index" do
+    auth_user 'admin_user'
+    it 'should return reservation' do
+      create(:reservation)
+      create(:reservation)
+      get :index
+      res = JSON.parse(response.body)
+      expect(res.length).to eq(2)
+    end
+  end
 
+  describe "show" do
+    auth_user 'admin_user'
+    it 'should return reservation' do
+      reservation = create(:reservation)
+      get :show, params: { id: reservation.id }
+      res = JSON.parse(response.body)
+      expect(res.dig('id')).to eq(reservation.id)
+    end
+  end
+
+  describe "update" do
+    auth_user 'admin_user'
+    it 'should update reservation data' do
+      reservation = create(:reservation)
+      patch :update, params: { id: reservation.id, reservation: { covers: ['Daimen'], start_time: '9 AM' } }
+      res = JSON.parse(response.body)
+      expect(res.dig('covers')).to eq(['Daimen'])
+    end
+  end
+
+  describe "create" do
+    auth_user 'admin_user'
+    it 'should create reservation' do
+      post :create, params: { reservation: { restaurant_id: create(:restaurant).id, user_id: create(:user).id, covers: ['Daimen'], start_time: '9 AM' } }
+      res = JSON.parse(response.body)
+      expect(res.dig('covers')).to eq(['Daimen'])
+    end
+  end
+
+  describe "destroy" do
+    auth_user 'admin_user'
+    it 'should delete reservation' do
+      reservation = create(:reservation)
+      delete :destroy, params: { id: reservation.id }
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
--- spec/controllers/api/v1/restaurants_controller_spec.rb
+++ spec/controllers/api/v1/restaurants_controller_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::RestaurantsController, type: :controller do
+  describe "index" do
+    auth_user 'admin_user'
+    it 'should return restaurants' do
+      create(:restaurant)
+      create(:restaurant)
+      get :index
+      res = JSON.parse(response.body)
+      expect(res.length).to eq(2)
+    end
+  end
 
+  describe "show" do
+    auth_user 'admin_user'
+    it 'should return restaurant' do
+      restaurant = create(:restaurant)
+      get :show, params: { id: restaurant.id }
+      res = JSON.parse(response.body)
+      expect(res.dig('id')).to eq(restaurant.id)
+    end
+  end
+
+  describe "update" do
+    auth_user 'admin_user'
+    it 'should update restaurant data' do
+      restaurant = create(:restaurant, name: 'KFC')
+      patch :update, params: { id: restaurant.id, restaurant: { name: 'Mac' } }
+      res = JSON.parse(response.body)
+      expect(res.dig('name')).to eq('Mac')
+    end
+  end
+
+  describe "create" do
+    auth_user 'admin_user'
+    it 'should create restaurant' do
+      post :create, params: { restaurant: { name: 'Mac', user_id: create(:user).id, location: 'UAE', opening_hours: '9 AM', cuisines: 'fast food' } }
+      res = JSON.parse(response.body)
+      expect(res.dig('name')).to eq('Mac')
+    end
+  end
+
+  describe "destroy" do
+    auth_user 'admin_user'
+    it 'should delete restaurant' do
+      restaurant = create(:restaurant)
+      delete :destroy, params: { id: restaurant.id }
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
