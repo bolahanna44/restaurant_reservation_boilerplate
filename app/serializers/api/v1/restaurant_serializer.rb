0dcaf7b660372f7f9f58aa6f252706d6dc00d555
poula <bola.hanna55@gmail.com>
2019-11-03 16:49:26 +0400

    add restaurant serializer

--- app/serializers/api/v1/restaurant_serializer.rb
+++ app/serializers/api/v1/restaurant_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :cuisines, :phone, :email, :location, :opening_hours
+  attributes :id, :name, :cuisines, :phone, :location, :opening_hours
 end
