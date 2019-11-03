dccf47bc39d040215a5fc9f7cca40cef51e8632c
poula <bola.hanna55@gmail.com>
2019-11-03 16:49:07 +0400

    remove authenticity token from api and edit devise token config

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::ApplicationController < ActionController::Base
   include Pundit
+  skip_before_action :verify_authenticity_token
 
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :authenticate_user!
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
 
   def user_not_authorized
     render json: { errors: 'not authorized' }, status: :unauthorized
--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,6 +2,7 @@ Apipie.configure do |config|
   config.app_name                = "Eatapp"
   config.api_base_url            = "/"
   config.doc_base_url            = "/apipie"
+  config.validate = false
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
   config.translate = false
   config.default_locale = nil
